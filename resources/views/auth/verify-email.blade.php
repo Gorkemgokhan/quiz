a5fb90aa3e099b73c1fcca048a5e3381b9729972
Gorkemgokhan <110126902+Gorkemgokhan@users.noreply.github.com>
2022-08-01 08:11:09 +0300

    Kullanıcı quiz sayfam

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,45 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('Devam etmeden önce, size az önce e-postayla gönderdiğimiz bağlantıya tıklayarak e-posta adresinizi doğrulayabilir misiniz? E-postayı almadıysanız, size memnuniyetle başka bir e-posta göndeririz.') }}
+        </div>
+
+        @if (session('status') == 'verification-link-sent')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ __('Profil ayarlarınızda belirttiğiniz e-posta adresine yeni bir doğrulama bağlantısı gönderildi.') }}
+            </div>
+        @endif
+
+        <div class="mt-4 flex items-center justify-between">
+            <form method="POST" action="{{ route('verification.send') }}">
+                @csrf
+
+                <div>
+                    <x-jet-button type="submit">
+                        {{ __('Doğrulama e-postasını tekrar gönder') }}
+                    </x-jet-button>
+                </div>
+            </form>
+
+            <div>
+                <a
+                    href="{{ route('profile.show') }}"
+                    class="underline text-sm text-gray-600 hover:text-gray-900"
+                >
+                    {{ __('Profili Düzenle') }}</a>
+
+                <form method="POST" action="{{ route('logout') }}" class="inline">
+                    @csrf
+
+                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 ml-2">
+                        {{ __('Çıkış Yap') }}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </x-jet-authentication-card>
+</x-guest-layout>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <x-slot name="header">AnaSayfa</x-slot>
+</x-app-layout>
